7ad28a919bee5ab277adcc2562f54c7803624608
Malini-1326 <malinism2005@gmail.com>
2026-03-07 20:45:16 +0530

    login and register

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,22 @@
-
+"use client";
 import Header from "@/components/Header/Header";
 import "./globals.css";
 import Footer from "@/components/Footer/Footer";
-
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+  const hideLayout = pathname === "/login" || pathname === "/register";
   return (
     <html lang="en">
       <body>
-         <Header />  
+         {!hideLayout && <Header />} 
         {children}
-        <Footer />
+        {!hideLayout && <Footer />}
       </body>
     </html>
   );
