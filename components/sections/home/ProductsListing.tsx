007a640b0a29a9d08737e06b9eea5736d7f98ebd
Malini-1326 <malinism2005@gmail.com>
2026-03-27 14:31:49 +0530

    product managment is completed

--- components/sections/home/ProductsListing.tsx
+++ components/sections/home/ProductsListing.tsx
@@ -38,7 +38,7 @@ const ProductsListing = ({ onQuickView }: { onQuickView: () => void }) => {
                     );
                 })}
             </div>
-        </section>
+        </section>                                     
     );
 };
 
